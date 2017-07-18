2b11d18c4f4d601d0a1fec5b323bd9e99f346f51
Bunny67 <zolov67@yandex.ru>
2017-07-18 18:21:39 +0300

    update #4

--- ElvUI_Enhanced/Modules/Misc/characterFrame.lua
+++ ElvUI_Enhanced/Modules/Misc/characterFrame.lua
@@ -76,7 +76,7 @@ PAPERDOLL_SIDEBARS = {
 	{
 		name = L["Titles"];
 		frame = "PaperDollTitlesPane";
-		icon = "Interface\\AddOns\\ElvUI_Bunny\\media\\textures\\PaperDollSidebarTabs";
+		icon = "Interface\\AddOns\\ElvUI_Enhanced\\Mmedia\\Textures\\PaperDollSidebarTabs";
 		texCoords = {0.01562500, 0.53125000, 0.32421875, 0.46093750};
 	}
 }
