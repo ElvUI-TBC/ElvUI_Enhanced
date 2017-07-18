e40b17530eb87b8f91f7ba7f72040931cba2a40a
Appolyonn <jellefenxd@gmail.com>
2017-07-18 11:38:55 +0300

    Update

--- ElvUI_Enhanced/modules/misc/questLevel.lua
+++ ElvUI_Enhanced/modules/misc/questLevel.lua
@@ -3,26 +3,19 @@ local E, L, V, P, G = unpack(ElvUI);
 local function questlevel()
 	if not E.db.general.showQuestLevel then return; end
 
-	QUESTS_DISPLAYED = 25
-
-	local scrollOffset = FauxScrollFrame_GetOffset(QuestLogListScrollFrame);
-	local numEntries, numQuests = GetNumQuestLogEntries();
-
-	for i = 7, 25 do
-		local questLogTitle = CreateFrame("Button", "QuestLogTitle" .. i, QuestLogFrame, "QuestLogTitleButtonTemplate")
-
-		questLogTitle:SetID(i)
-		questLogTitle:Hide()
-		questLogTitle:Point("TOPLEFT", _G["QuestLogTitle" .. i - 1], "BOTTOMLEFT", 0, 1)
-	end
+	local scrollOffset = FauxScrollFrame_GetOffset(QuestLogListScrollFrame)
+	local numEntries = GetNumQuestLogEntries()
 
 	for i = 1, QUESTS_DISPLAYED do
 		local questIndex = i + scrollOffset;
 		local questLogTitle = _G["QuestLogTitle" .. i]
-		if(questIndex <= numEntries) then
-			local title, level, _, _, isHeader = GetQuestLogTitle(questIndex);
-			if(not isHeader) then
-				questLogTitle:SetText("[" .. level .. "] " .. title);
+		local questCheck = _G["QuestLogTitle"..i.."Check"]
+
+		if questIndex <= numEntries then
+			local title, level, _, _, isHeader = GetQuestLogTitle(questIndex)
+			if not isHeader then
+				questLogTitle:SetText("[" .. level .. "] " .. title)
+				questCheck:Point("LEFT", 5, 0)
 			end
 		end
 	end
