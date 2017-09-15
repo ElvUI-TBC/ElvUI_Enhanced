11a991ec39b69ef0c3546aa92324b6d8dbbc503d
Apollyonn <jellefenxd@gmail.com>
2017-09-15 14:33:23 +0300

    update config

--- ElvUI_Enhanced/Locales/Korean.lua
+++ ElvUI_Enhanced/Locales/Korean.lua
@@ -40,10 +40,11 @@ L["Enable/Disable the display of durability information on the character screen.
 L["Enable/Disable the display of item levels on the character screen."] = true;
 L["Enhanced Character Frame"] = true;
 L["Equipment"] = true;
-L["Inspect Paperdoll Background"] = true;
+L["Inspect Background"] = true;
 L["Only show durabitlity information for items that are damaged."] = true;
-L["Paperdoll Background"] = true;
-L["Pet Paperdoll Background"] = true;
+L["Paperdoll Backgrounds"] = true;
+L["Character Background"] = true;
+L["Pet Background"] = true;
 L["Quality Color"] = true;
 
 -- Datatext
