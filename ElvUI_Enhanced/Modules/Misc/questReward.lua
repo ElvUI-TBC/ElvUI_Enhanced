0ba01795087b1fdb2ac688b3700f2b59ff10d02a
Apollyonn <jellefenxd@gmail.com>
2018-10-07 08:57:06 +0300

    update

--- ElvUI_Enhanced/Modules/Misc/questReward.lua
+++ ElvUI_Enhanced/Modules/Misc/questReward.lua
@@ -8,17 +8,17 @@ local format = string.format
 local function SelectQuestReward(index)
 	local btn = _G[("QuestRewardItem%d"):format(index)]
 
-	--if btn.type == "choice" then
+	if btn.type == "choice" then
 		QuestRewardItemHighlight:ClearAllPoints()
-		QuestRewardItemHighlight:SetAllPoints(this)
+		QuestRewardItemHighlight:SetAllPoints(btn)
 		QuestRewardItemHighlight:Show()
 
-		QuestFrameRewardPanel.itemChoice = this:GetID()
-	--end
+		QuestFrameRewardPanel.itemChoice = btn:GetID()
+	end
 end
 
 function M:QUEST_COMPLETE()
-	--if not E.db.enhanced.general.selectQuestReward then return end
+	if not E.db.enhanced.general.selectQuestReward then return end
 
 	local choice, price = 1, 0
 	local num = GetNumQuestChoices()
@@ -28,9 +28,9 @@ function M:QUEST_COMPLETE()
 	for index = 1, num do
 		local link = GetQuestItemLink("choice", index)
 		if link then
-			local vsp = LIP:GetSellValue(link)
-			if vsp and vsp > price then
-				price = vsp
+			local itemPrice = LIP:GetSellValue(link)
+			if itemPrice and itemPrice > price then
+				price = itemPrice
 				choice = index
 			end
 		end
