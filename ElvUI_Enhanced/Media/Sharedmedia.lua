899e83d909ff2b086674b1b76313a80dbbdf529c
Apollyonn <jellefenxd@gmail.com>
2018-11-03 04:14:06 +0200

    update

--- ElvUI_Enhanced/Media/Sharedmedia.lua
+++ ElvUI_Enhanced/Media/Sharedmedia.lua
@@ -1,3 +1,4 @@
 local LSM = LibStub("LibSharedMedia-3.0")
 
-LSM:Register("font", "TukUI Unitframes", [[Interface\Addons\ElvUI_Enhanced\Media\fonts\uf_font.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western);
+LSM:Register("font", "TukUI Unitframes", [[Interface\Addons\ElvUI_Enhanced\Media\Fonts\uf_font.ttf]], LSM.LOCALE_BIT_ruRU + LSM.LOCALE_BIT_western)
+LSM:Register("statusbar", "Bui OnePixel", [[Interface\AddOns\ElvUI_Enhanced\Media\Textures\BuiOnePixel.tga]])
